fcef04fe6b2aea058d40e89b0085815957f8b6be
Your Name <you@example.com>
2021-04-02 13:01:07 +0900

    ex04

---
+++ cpp01/ex04/ex04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str("HI THIS IS BRAIN");
+	std::string *ptr;
+	std::string &ref = str;
+
+	ptr = &str;
+	std::cout << *ptr << std::endl;
+	std::cout << ref << std::endl;
+}
